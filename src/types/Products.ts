9417f0635c95fec42b18beb66eaf1eb309fb0c1f
Carla Sofia Martins <carla.s.ofia@hotmail.com>
2026-02-15 16:08:30 +0000

    Added omit to types.ts

--- src/types/Products.ts
+++ src/types/Products.ts
@@ -1,37 +1,16 @@
-import type { IProducts } from "./types"; //importo o type IProducts
+import type { IProduct, IProductSummary } from "./types";
 
-interface IProductEach {
-  //representa exatamente como a API retorna os dados
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  category: string;
-  image: string;
-  rating: {
-    rate: number;
-    count: number;
-  };
-}
+export const fetchProducts = async (): Promise<IProductSummary[]> => {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const data: IProduct[] = await response.json();
 
-export const fetchProducts = async (): Promise<IProducts> => {
-  try {
-    const response = await fetch("https://fakestoreapi.com/products");
-    const data: IProductEach[] = await response.json(); //data é do tipo IProductEach que tem array
-
-    // Retorna apenas os campos da lista, sem rate
-    return data.map((product) => ({
-      //o data do tipo anterior, produra em cada item e atua em cada um.
-      id: product.id,
-      title: product.title,
-      price: product.price,
-      description: product.description,
-      category: product.category,
-      image: product.image,
-      // Aqui não adiciono rate, porque não quero mostrar nos cards dos productos
-    }));
-  } catch (error) {
-    console.error("Erro ao buscar produtos:", error);
-    return [];
-  }
+  return data.map((item) => ({
+    id: item.id,
+    title: item.title,
+    price: item.price,
+    description: item.description,
+    category: item.category,
+    image: item.image,
+    // Se tentasses colocar 'rate' aqui, o TypeScript daria erro!
+  }));
 };
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,16 +1,17 @@
-export type IProducts = IProduct[];
-
 export interface IProduct {
-  //Este tipo define o que cada produto tem.
   id: number;
   title: string;
   price: number;
   description: string;
   category: string;
   image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
 }
 
-export interface IProductRate extends IProduct {
-  //aqui adiciona um novo parametro, não mexendo no anterior, apenas adiciona quando chamado. Ou seja, Em cada IProduct, vai ser adicionado o rate
+export type IProductSummary = Omit<IProduct, "rating">;
+export interface IProductDetail extends IProductSummary {
   rate: number;
 }
